58e6296cfd1dead7d020839bd5927edf0e9af301
eltayeb <gaaliii@yahoo.com>
2020-07-29 17:37:56 +0200

    update-add-item-listener closes #3

--- scripts/listeners/add-item.js
+++ scripts/listeners/add-item.js
@@ -2,4 +2,6 @@
 console.log('--- loading listener: add-item');
 
 const addItems = document.querySelector('.add-items');
+const itemsList = document.querySelector('.plates');
+
 addItems.addEventListener('submit', addItemHandler);
